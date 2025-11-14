e8e77adaa2de9030f42a22c6d5c6aa28922165bc
Vinicius Dos Santos Girotti <vinicius0621@hotmail.com>
2025-11-14 11:35:38 -0300

    Update csv-exporter.ts
    Buscando os informações ja armazenadas no banco de dados referente a cada tabela para exportação via CSV

--- src/csv-exporter.ts
+++ src/csv-exporter.ts
@@ -1,2 +1,10 @@
 import { db } from "NOTA10";
 import { Parser } from "csv"
+
+export async function gerarCSVCompleto() {
+  
+// 1. Buscar dados das tabelas existentes//
+const alunos = await db.query("SELECT * FROM alunos");
+const turmas = await db.query("SELECT * FROM turmas");
+const disciplinas = await db.query("SELECT * FROM disciplinas");
+const notas = await db.query("SELECT * FROM notas");
